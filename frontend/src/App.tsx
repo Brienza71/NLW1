507dcb57090df73c01d311e2dea55fc7f3fa1163
Leonardo Brienza <54857744+Brienza71@users.noreply.github.com>
2020-10-14 17:08:14 -0300

    Add files via upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,9 @@
 import React from 'react';
 
-import './styles/global.css';
-import './styles/pages/landing.css';
-
-import logoImg from './images/Logo.svg';
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy"></img>
-        <main>
-          <h1>Leve felicidade para o mundo!</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className="location">
-          <strong>Indaiatuba</strong>
-          <span>São Paulo</span>
-        </div>
-
-        <a href="" className="enter-app">
-          >
-        </a>
-
-      </div>
-    </div>
+    <h1>Hello</h1>
   );
 }
 
